9d73e866b1902f3c84416aad5ba96e80ce212d12
shivujg <162001736+shivujg@users.noreply.github.com>
2024-04-07 23:55:10 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint256 public totalCreditPoints;
+    string public userName;
+    string public college;
+    string public course;
+    string public branch;
+    uint256 public dateOfOpening;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
@@ -13,48 +17,58 @@ contract Assessment {
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
+        totalCreditPoints = 0;
+        dateOfOpening = block.timestamp;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
+        if (_amount > 100 || totalCreditPoints >= 100) {
+            totalCreditPoints += 100;
+        }
+        assert(balance == _previousBalance + _amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
                 withdrawAmount: _withdrawAmount
             });
         }
-
-        // withdraw the given amount
+        uint _previousBalance = balance;
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+        if (_withdrawAmount > 100 || totalCreditPoints >= 100) {
+            totalCreditPoints += 100;
+        }
+        assert(balance == (_previousBalance - _withdrawAmount));
+    }
+
+    function getCreditPoints() public view returns (uint256) {
+        if (balance >= 100 || totalCreditPoints >= 100) {
+            return 100;
+        } else {
+            return 0;
+        }
+    }
+
+    function getUserInfo() public view returns (string memory, string memory, string memory, string memory, uint256) {
+        return (userName, college, course, branch, dateOfOpening);
+    }
+
+    function setUserInfo(string memory _userName, string memory _college, string memory _course, string memory _branch) public {
+        userName = _userName;
+        college = _college;
+        course = _course;
+        branch = _branch;
     }
 }
